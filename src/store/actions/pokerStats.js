29935471e2347aff6a47aaf8c398f88100420141
Mensur <mensurdurakovic5@gmail.com>
2020-06-22 00:26:24 +0200

    cleanups

--- src/store/actions/pokerStats.js
+++ src/store/actions/pokerStats.js
@@ -1,7 +1,6 @@
 import * as actionTypes from "./actionTypes";
 
 export const setActiveDeskCard = (place, position) => {
-  console.log(setActiveDeskCard);
   return {
     type: actionTypes.SET_ACTIVE_DESK_CARD,
     place: place,
--- src/store/reducers/pokerStats.js
+++ src/store/reducers/pokerStats.js
@@ -2,43 +2,43 @@ import * as actionTypes from "../actions/actionTypes";
 import { updateObject } from "../../utils/reduxUtils";
 
 const cleanDesk = {
-  player1: { cards: ['', ''], active: [0, 1] },
-    player2: { cards: ['', ''], active: [0, 0]  },
-    player3: { cards: ['', ''], active: [0, 0] },
-    player4: { cards: ['', ''], active: [0, 0]  },
-    player5: { cards: ['', ''], active: [0, 0]  },
-    player6: { cards: ['', ''], active: [0, 0]  },
-    player7: { cards: ['', ''], active: [0, 0]  },
-    player8: { cards: ['', ''], active: [0, 0]  },
-    player9: { cards: ['', ''], active: [0, 0]  },
-    desk: { cards: ['', '', '', '', ''] ,active: [0, 0, 0, 0, 0] },
+  player1: { cards: ["", ""], active: [0, 1] },
+  player2: { cards: ["", ""], active: [0, 0] },
+  player3: { cards: ["", ""], active: [0, 0] },
+  player4: { cards: ["", ""], active: [0, 0] },
+  player5: { cards: ["", ""], active: [0, 0] },
+  player6: { cards: ["", ""], active: [0, 0] },
+  player7: { cards: ["", ""], active: [0, 0] },
+  player8: { cards: ["", ""], active: [0, 0] },
+  player9: { cards: ["", ""], active: [0, 0] },
+  desk: { cards: ["", "", "", "", ""], active: [0, 0, 0, 0, 0] },
 };
 
 const initialState = {
   deskState: cleanDesk,
   pickedCardId: "",
-  activePlace: 'player1',
-  activePosition: 1
+  activePlace: "player1",
+  activePosition: 1,
 };
 
 const pickCard = (state, action) => {
   const newDeskState = {
     player1: { cards: [...state.deskState.player1.cards], active: [0, 0] },
-    player2: { cards: [...state.deskState.player2.cards], active: [0, 0]  },
+    player2: { cards: [...state.deskState.player2.cards], active: [0, 0] },
     player3: { cards: [...state.deskState.player3.cards], active: [0, 0] },
-    player4: { cards: [...state.deskState.player4.cards], active: [0, 0]  },
-    player5: { cards: [...state.deskState.player5.cards], active: [0, 0]  },
-    player6: { cards: [...state.deskState.player6.cards], active: [0, 0]  },
-    player7: { cards: [...state.deskState.player7.cards], active: [0, 0]  },
-    player8: { cards: [...state.deskState.player8.cards], active: [0, 0]  },
-    player9: { cards: [...state.deskState.player9.cards], active: [0, 0]  },
+    player4: { cards: [...state.deskState.player4.cards], active: [0, 0] },
+    player5: { cards: [...state.deskState.player5.cards], active: [0, 0] },
+    player6: { cards: [...state.deskState.player6.cards], active: [0, 0] },
+    player7: { cards: [...state.deskState.player7.cards], active: [0, 0] },
+    player8: { cards: [...state.deskState.player8.cards], active: [0, 0] },
+    player9: { cards: [...state.deskState.player9.cards], active: [0, 0] },
     desk: { cards: [...state.deskState.desk.cards], active: [0, 0, 0, 0, 0] },
   };
   newDeskState[state.activePlace].active[state.activePosition] = 0;
   newDeskState[state.activePlace].cards[state.activePosition] = action.cardId;
   return updateObject(state, {
     pickedCardId: action.cardId,
-    deskState: newDeskState
+    deskState: newDeskState,
   });
 };
 
@@ -46,15 +46,15 @@ const setActiveCard = (state, action) => {
   console.log(action);
   const newDeskState = {
     player1: { cards: [...state.deskState.player1.cards], active: [0, 0] },
-    player2: { cards: [...state.deskState.player2.cards], active: [0, 0]  },
+    player2: { cards: [...state.deskState.player2.cards], active: [0, 0] },
     player3: { cards: [...state.deskState.player3.cards], active: [0, 0] },
-    player4: { cards: [...state.deskState.player4.cards], active: [0, 0]  },
-    player5: { cards: [...state.deskState.player5.cards], active: [0, 0]  },
-    player6: { cards: [...state.deskState.player6.cards], active: [0, 0]  },
-    player7: { cards: [...state.deskState.player7.cards], active: [0, 0]  },
-    player8: { cards: [...state.deskState.player8.cards], active: [0, 0]  },
-    player9: { cards: [...state.deskState.player9.cards], active: [0, 0]  },
-    desk: { cards: [...state.deskState.desk.cards] ,active: [0, 0, 0, 0, 0] },
+    player4: { cards: [...state.deskState.player4.cards], active: [0, 0] },
+    player5: { cards: [...state.deskState.player5.cards], active: [0, 0] },
+    player6: { cards: [...state.deskState.player6.cards], active: [0, 0] },
+    player7: { cards: [...state.deskState.player7.cards], active: [0, 0] },
+    player8: { cards: [...state.deskState.player8.cards], active: [0, 0] },
+    player9: { cards: [...state.deskState.player9.cards], active: [0, 0] },
+    desk: { cards: [...state.deskState.desk.cards], active: [0, 0, 0, 0, 0] },
   };
   const oldValue = state.deskState[action.place].active[action.position];
   newDeskState[action.place].active[action.position] = oldValue === 1 ? 0 : 1;
@@ -68,16 +68,16 @@ const setActiveCard = (state, action) => {
 
 const resetCards = (state, action) => {
   const newDeskState = {
-    player1: { cards: ['', ''], active: [0, 0] },
-    player2: { cards: ['', ''], active: [0, 0] },
-    player3: { cards: ['', ''], active: [0, 0] },
-    player4: { cards: ['', ''], active: [0, 0] },
-    player5: { cards: ['', ''], active: [0, 0] },
-    player6: { cards: ['', ''], active: [0, 0] },
-    player7: { cards: ['', ''], active: [0, 0] },
-    player8: { cards: ['', ''], active: [0, 0] },
-    player9: { cards: ['', ''], active: [0, 0] },
-    desk: { cards: ['', '', '', '', ''] ,active: [0, 0, 0, 0, 0] },
+    player1: { cards: ["", ""], active: [0, 0] },
+    player2: { cards: ["", ""], active: [0, 0] },
+    player3: { cards: ["", ""], active: [0, 0] },
+    player4: { cards: ["", ""], active: [0, 0] },
+    player5: { cards: ["", ""], active: [0, 0] },
+    player6: { cards: ["", ""], active: [0, 0] },
+    player7: { cards: ["", ""], active: [0, 0] },
+    player8: { cards: ["", ""], active: [0, 0] },
+    player9: { cards: ["", ""], active: [0, 0] },
+    desk: { cards: ["", "", "", "", ""], active: [0, 0, 0, 0, 0] },
   };
   return updateObject(state, {
     deskState: newDeskState,
